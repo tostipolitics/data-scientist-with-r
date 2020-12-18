6bf96666f75f48e4cb37e92514dd5da37daa684a
tostipolitics <jstrujillo.andes@gmail.com>
2020-12-18 11:05:52 -0500

    Introduction to R

---
+++ Scripts/introduction-to-r.R
@@ -0,0 +1,32 @@
+#INTRODUCTION TO R
+#Intro to basics: operadores aritméticos, asignación de variables, tipos de datos, 
+#Vectors
+#Matrices
+#Factors
+#Dataframes
+#Lists
+
+#-------------------------------------------------------------------------------
+
+#Intro to basics
+
+#Operadores aritméticos
++
+-
+*
+/
+^
+%% #residuo
+  
+#Asignación de variables
+x<-10
+
+#Tipos de datos
+numeric()
+integer()
+logical()
+character()
+class()
+
+#-------------------------------------------------------------------------------
+
